c1f6558680f7e8feeaea8b47fed4b4d2555054ce
wwbweibo <wwbweibo@outlook.com>
2020-09-04 14:15:46 +0800

    添加Des加解密

---
+++ Wwbweibo.Utils.Tests/Crypo/DesHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Extensions.Utils;
+using Wwbweibo.Utils.Crypto;
+
+namespace Wwbweibo.Utils.Crypto.Tests
+{
+    [TestClass()]
+    public class DesHelperTests
+    {
+        [TestMethod()]
+        public void DesDecryptTest()
+        {
+            var text = "abc";
+            var key = "111";
+
+            var encoded = DesHelper.DesEncrypt(text.ToBytes(), key.ToBytes(), new byte[0]);
+            var decoded = DesHelper.DesDecrypt(encoded, key.ToBytes(), new byte[0]);
+            Assert.AreEqual(text, decoded.GetString());
+        }
+    }
+}
---
+++ Wwbweibo.Utils/Crypto/DesHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Wwbweibo.Utils.Crypto
+{
+    public class DesHelper
+    {
+        public static byte[] DesEncrypt(byte[] data, byte[] key, byte[] IV)
+        {
+            using var provider = new DESCryptoServiceProvider();
+
+            provider.Key = HandleKey(key);
+            provider.IV = IV.Length == 8 ? IV : new byte[8];
+            using var ct = provider.CreateEncryptor();
+            using var ms = new MemoryStream();
+            using var cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+            cs.Write(data, 0, data.Length);
+            cs.FlushFinalBlock();
+            return ms.ToArray();
+        }
+
+        public static byte[] DesDecrypt(byte[] data, byte[] key, byte[] IV)
+        {
+            using var provider = new DESCryptoServiceProvider();
+            provider.Key = HandleKey(key);
+            provider.IV = IV.Length == 8 ? IV : new byte[8];
+            using var ct = provider.CreateDecryptor();
+            using var ms = new MemoryStream();
+            using var cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+            cs.Write(data, 0, data.Length);
+            cs.FlushFinalBlock();
+            return ms.ToArray();
+        }
+
+        private static byte[] HandleKey(byte[] key)
+        {
+            byte[] k;
+            if (key.Length <= 8)
+            {
+                k = new byte[8];
+                key.CopyTo(k, 0);
+            }
+            else
+            {
+                throw new ArgumentException("the given key is to long");
+            }
+            return k;
+        }
+    }
+}
