e967675771f6d7ab975557e40e8b7ffbe9e06393
wwbweibo <wwbweibo@outlook.com>
2020-09-12 12:25:27 +0800

    添加short和long类型的转换方法

--- Wwbweibo.Utils/Extensions/StringExtensions.cs
+++ Wwbweibo.Utils/Extensions/StringExtensions.cs
@@ -20,32 +20,7 @@ namespace Wwbweibo.Utils.Extensions
             return string.IsNullOrEmpty(str);
         }
 
-        public static int ToInt(this string str, int value = 0, bool throwException = false)
-        {
-            if (int.TryParse(str, out var num))
-                return num;
-            else if (throwException)
-                throw new FormatException("the input value is not valid");
-            return value;
-        }
-
-        public static float ToFloat(this string str, float value = 0, bool throwException = false)
-        {
-            if (float.TryParse(str, out var num))
-                return num;
-            else if (throwException)
-                throw new FormatException("the input value is not valid");
-            return value;
-        }
-
-        public static double ToDouble(this string str, double value = 0, bool throwException = false)
-        {
-            if (double.TryParse(str, out var num))
-                return num;
-            else if (throwException)
-                throw new FormatException("the input value is not valid");
-            return value;
-        }
+        
 
         public static string Base64Encode(this string str, string encoding = "UTF-8")
         {
---
+++ Wwbweibo.Utils/Extensions/StringValueParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wwbweibo.Utils.Extensions
+{
+    public static class  StringValueParser
+    {
+        public static short ToShort(this string str, short value = 0, bool throwException = false)
+        {
+            if (short.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+
+        public static int ToInt(this string str, int value = 0, bool throwException = false)
+        {
+            if (int.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+
+        public static long ToLong(this string str, long value = 0, bool throwException = false)
+        {
+            if (long.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+
+        public static float ToFloat(this string str, float value = 0, bool throwException = false)
+        {
+            if (float.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+
+        public static double ToDouble(this string str, double value = 0, bool throwException = false)
+        {
+            if (double.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+    }
+}
