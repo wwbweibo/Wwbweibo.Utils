a689b0b93fb8ea513a656ba92c9a1f7eeb8704a9
wwbweibo <wwbweibo@outlook.com>
2020-10-17 14:05:44 +0800

    添加无符号整型转换

--- Wwbweibo.Utils/Extensions/StringValueParser.cs
+++ Wwbweibo.Utils/Extensions/StringValueParser.cs
@@ -15,9 +15,28 @@ namespace Wwbweibo.Utils.Extensions
             return value;
         }
 
+        public static ushort ToUShort(this string str, ushort value = 0, bool throwException = false)
+        {
+            if (ushort.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+
+
         public static int ToInt(this string str, int value = 0, bool throwException = false)
         {
             if (int.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+
+        public static uint ToUInt(this string str, uint value = 0, bool throwException = false)
+        {
+            if (uint.TryParse(str, out var num))
                 return num;
             else if (throwException)
                 throw new FormatException("the input value is not valid");
@@ -32,6 +51,16 @@ namespace Wwbweibo.Utils.Extensions
                 throw new FormatException("the input value is not valid");
             return value;
         }
+
+        public static ulong ToULong(this string str, ulong value = 0, bool throwException = false)
+        {
+            if (ulong.TryParse(str, out var num))
+                return num;
+            else if (throwException)
+                throw new FormatException("the input value is not valid");
+            return value;
+        }
+
 
         public static float ToFloat(this string str, float value = 0, bool throwException = false)
         {
