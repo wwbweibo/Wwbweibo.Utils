430eccb2f6b0591e38809e97e5f3286b718489e5
wwbweibo <wwbweibo@outlook.com>
2020-10-17 09:25:23 +0800

    添加Md5计算方法

---
+++ Wwbweibo.Utils.Tests/Crypto/HashHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wwbweibo.Utils.Crypto;
+using Wwbweibo.Utils.Extensions;
+
+namespace Wwbweibo.Utils.Tests.Crypto
+{
+    [TestClass]
+    public class HashHelperTests
+    {
+        
+        [TestMethod]
+        public void Md5Test()
+        {
+            var expected = "900150983CD24FB0D6963F7D28E17F72";
+            Assert.AreEqual(expected, HashHelper.Md5Checksum("abc".ToBytes()));
+        }
+    }
+}
